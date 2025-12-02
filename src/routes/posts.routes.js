a66aef43d9de7769ff35f86f4b5927dd0949c872
pranavranjith <pranavranjith007@gmail.com>
2025-12-02 09:34:06 +0530

    refactor: move post logic to controller

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// 1. IMPORT THE CONTROLLER
+
 const postController = require('../controllers/posts.controller.js');
 
-// 2. USE THE CONTROLLER FUNCTION
+
 router.get('/', postController.getAllPosts);
 
 module.exports = router;
